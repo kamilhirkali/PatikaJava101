0a0e281979e019490c75e272378f76cad24d24ac
Kamil Hırkalı <kmlhrkl@gmail.com>
2023-03-15 10:19:58 +0300

    V5

---
+++ src/main/java/odevler/Odev6_VucutKitleIndeksiHesaplama.java
@@ -0,0 +1,26 @@
+package odevler;
+
+import java.util.Scanner;
+
+public class Odev6_VucutKitleIndeksiHesaplama {
+/*Vücut Kitle İndeksi Hesaplama
+Java ile kullanıcıdan boy ve kilo değerlerini alıp bir değişkene atayın. Aşağıdaki formüle göre kullanıcının "Vücut Kitle İndeks" değerini hesaplayıp ekrana yazdırın.
+
+Formül
+Kilo (kg) / Boy(m) * Boy(m)
+*/
+
+    public static void main(String[] args) {
+        double kg,boy,vki;
+        Scanner input =new Scanner(System.in);
+        System.out.println("Kilo degerini giriniz : ");
+        kg= input.nextDouble();
+        System.out.println("Boy degerini giriniz : ");
+        boy= input.nextDouble();
+        vki=kg/(boy*boy);
+        System.out.println("Vucut Kitle indeksiniz = " + vki);
+
+
+    }
+
+}
