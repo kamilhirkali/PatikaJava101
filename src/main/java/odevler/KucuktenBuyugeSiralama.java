a128bde862af4dae424ef1c40d30b9723fd761a3
Kamil Hırkalı <kmlhrkl@gmail.com>
2023-03-19 10:03:21 +0300

    V10

---
+++ src/main/java/odevler/KucuktenBuyugeSiralama.java
@@ -0,0 +1,41 @@
+package odevler;
+
+import java.util.Scanner;
+
+public class KucuktenBuyugeSiralama {
+    /*Girilen 3 sayıyı "küçükten büyüğe" sıralayan programı yazınız.*/
+    public static void main(String[] args) {
+        int a,b,c;
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("a sayisini giriniz : ");
+        a=input.nextInt();
+
+        System.out.print("b sayisini giriniz : ");
+        b=input.nextInt();
+
+        System.out.print("c sayisini giriniz : ");
+        c=input.nextInt();
+
+
+        if(a<b && a<c) {
+            if (b < c) {
+                System.out.println("a<b<c");
+            } else {
+                System.out.println("a<c<b");
+            }
+        }else if(b<a && b<c) {
+            if (a < c) {
+                System.out.println("b<a<c");
+            } else {
+                System.out.println("b<c<a");
+            }
+        }else{
+            if (a < b) {
+                System.out.println("c<a<b");
+            } else {
+                System.out.println("c<b<a");
+            }
+        }
+    }
+}
