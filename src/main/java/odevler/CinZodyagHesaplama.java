920457d6b0c16099307f7b355643c384db48fea5
Kamil Hırkalı <kmlhrkl@gmail.com>
2023-03-19 23:46:37 +0300

    V10

--- src/main/java/odevler/CinZodyagHesaplama.java
+++ src/main/java/odevler/CinZodyagHesaplama.java
@@ -25,49 +25,53 @@ Doğum Tarihi %12 = 11 ➜ Koyun*/
         Scanner input=new Scanner(System.in);
         System.out.print("Doğum Yılınızı Giriniz :  ");
         dTarihi=input.nextInt();
-        sonuc=dTarihi%12;
+       if(dTarihi>1900 && dTarihi<=2023) {
+           sonuc = dTarihi % 12;
 
-        switch(sonuc){
+           switch (sonuc) {
 
-            case 0:
-                burc="Maymun";
-                break;
-            case 1:
-                burc="Horoz";
-                break;
-            case 2:
-                burc="Köpek";
-                break;
-            case 3:
-                burc="Domuz";
-                break;
-            case 4:
-                burc="Fare";
-                break;
-            case 5:
-                burc="Öküz";
-                break;
-            case 6:
-                burc="Kaplan";
-                break;
-            case 7:
-                burc="Tavşan";
-                break;
-            case 8:
-                burc="Ejderha";
-                break;
-            case 9:
-                burc="Yılan";
-                break;
-            case 10:
-                burc="At";
-                break;
-            case 11:
-                burc="Koyun";
-                break;
-            default:
-                isError=true;
-        }
+               case 0:
+                   burc = "Maymun";
+                   break;
+               case 1:
+                   burc = "Horoz";
+                   break;
+               case 2:
+                   burc = "Köpek";
+                   break;
+               case 3:
+                   burc = "Domuz";
+                   break;
+               case 4:
+                   burc = "Fare";
+                   break;
+               case 5:
+                   burc = "Öküz";
+                   break;
+               case 6:
+                   burc = "Kaplan";
+                   break;
+               case 7:
+                   burc = "Tavşan";
+                   break;
+               case 8:
+                   burc = "Ejderha";
+                   break;
+               case 9:
+                   burc = "Yılan";
+                   break;
+               case 10:
+                   burc = "At";
+                   break;
+               case 11:
+                   burc = "Koyun";
+                   break;
+               default:
+                   isError = true;
+           }
+       }else{
+               isError=true;
+           }
         if(isError){
             System.out.println("girilen deger gecersizdir");
         }else {
